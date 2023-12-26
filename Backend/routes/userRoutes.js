4df74c1257a8cac5fa0e9577f4ab0e14cdcbff41
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2023-12-26 20:55:07 +0530

    s

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -10,7 +10,30 @@ router.post('/register', async(req, res)=>{
         await user.save();
         const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
         res.status(201).send({ user, token });
+        console.log("User Register Successfully")
     } catch (error) {
         res.status(400).send(error);
+        console.log("Error While registering")
     }
 })
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).send('Unable to login');
+        }
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isMatch) {
+            return res.status(400).send('Unable to login');
+        }
+        const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+        res.send({ user, token });
+        console.log("Login Successfully")
+    } catch (error) {
+        res.status(400).send(error);
+        console.log("Login failed")
+    }
+});
+
+module.exports = router;
