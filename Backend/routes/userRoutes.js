7dbcb7cb95bbcb2fddbfa317b979627615ded238
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2023-12-26 20:50:33 +0530

    As

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+router.post('/register', async(req, res)=>{
+    try {
+        const user = new User(req.body);
+        await user.save();
+        const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+        res.status(201).send({ user, token });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
